c9c84ae25a7f060f5396e3466bf7c5125e58104e
DươngML <94815721+Duongbe@users.noreply.github.com>
2025-02-21 10:35:00 +0700

    Add files via upload

---
+++ lich_su_cong_tac.py
@@ -0,0 +1,19 @@
+from odoo import models, fields, api
+
+
+class LichSuCongTac(models.Model):
+    _name = 'lich_su_cong_tac'
+    _description = 'Lịch sử công tác'
+
+    nhan_vien_id = fields.Many2one("nhan_vien",string="Nhân viên", required=True)
+    phong_ban_id = fields.Many2one("phong_ban",string="Phòng ban", required=True)
+    chuc_vu_id = fields.Many2one("chuc_vu",string="Chức vụ", required=True)
+    loai_chuc_vu = fields.Selection(
+        [
+            ("Chính", "Chính"),
+            ("Kiêm nhiệm", "Kiêm nhiệm")
+        ],
+        string= "loại chức vụ", default = "Chính"
+    )
+
+
